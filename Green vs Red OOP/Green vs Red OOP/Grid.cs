3d2ccd898aef12220d88da5ec09eaf3a2572a08c
Peter-Lazarov <peter_lazarov@outlook.com>
2020-07-23 19:36:38 +0300

    Add files via upload

---
+++ Green vs Red OOP/Green vs Red OOP/Grid.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Green_vs_Red_OOP
+{
+    public class Grid
+    {
+        public Grid(int x, int y)
+        {
+            this.allNumbers = new int[x, y];
+            this.forNextGeneration = new int[x, y];
+            this.xLength = x;
+            this.yLength = y;
+        }
+
+        public int xLength { get; private set; }
+        public int yLength { get; private set; }
+
+        public int[,] allNumbers { get; set; }
+        private int[,] forNextGeneration { get; set; }
+
+        public void SetTheGridGenerationZero()
+        {
+            Console.WriteLine("Please write the first lines of the grid without separators (1010)");
+            for (int y = 0; y < yLength; y++)
+            {
+                Console.WriteLine(yLength - y + " line left");
+                string currentLine = Console.ReadLine();
+                for (int x = 0; x < xLength; x++)
+                {
+                    this.allNumbers[y, x] = currentLine[x] - '0';
+                }
+            }
+            Array.Copy(this.allNumbers, this.forNextGeneration, this.allNumbers.Length);
+        }
+
+        public void CalculateNextGeneration()
+        {
+            for (int column = 0; column < yLength; column++)
+            {
+                for (int row = 0; row < xLength; row++)
+                {
+                    bool rowAbove = false;
+                    bool rowBelow = false;
+
+                    bool columnLeft = false;
+                    bool columnRight = false;
+                    int greenCellsCount = 0;
+
+                    if (column - 1 > -1)
+                    {
+                        rowAbove = true;
+                    }
+
+                    if (column + 1 < xLength)
+                    {
+                        rowBelow = true;
+                    }
+
+                    if (row - 1 > -1)
+                    {
+                        columnLeft = true;
+                    }
+
+                    if (row + 1 < yLength)
+                    {
+                        columnRight = true;
+                    }
+
+                    if (rowAbove && rowBelow)
+                    {
+                        if (columnLeft && columnRight)
+                        {
+                            //full check - return count of green cells
+                            greenCellsCount += this.allNumbers[column - 1, row - 1];
+                            greenCellsCount += this.allNumbers[column - 1, row];
+                            greenCellsCount += this.allNumbers[column - 1, row + 1];
+                            greenCellsCount += this.allNumbers[column, row - 1];
+                            greenCellsCount += this.allNumbers[column, row + 1];
+                            greenCellsCount += this.allNumbers[column + 1, row - 1];
+                            greenCellsCount += this.allNumbers[column + 1, row];
+                            greenCellsCount += this.allNumbers[column + 1, row + 1];
+                        }
+                        else if (columnLeft)
+                        {
+                            greenCellsCount += this.allNumbers[column - 1, row - 1];
+                            greenCellsCount += this.allNumbers[column - 1, row];
+                            greenCellsCount += this.allNumbers[column, row - 1];
+                            greenCellsCount += this.allNumbers[column + 1, row - 1];
+                            greenCellsCount += this.allNumbers[column + 1, row];
+                        }
+                        else if (columnRight)
+                        {
+                            greenCellsCount += this.allNumbers[column - 1, row];
+                            greenCellsCount += this.allNumbers[column - 1, row + 1];
+                            greenCellsCount += this.allNumbers[column, row + 1];
+                            greenCellsCount += this.allNumbers[column + 1, row];
+                            greenCellsCount += this.allNumbers[column + 1, row + 1];
+                        }
+                        else
+                        {
+                            greenCellsCount += this.allNumbers[column - 1, row];
+                            greenCellsCount += this.allNumbers[column + 1, row];
+                        }
+                    }
+                    else if (rowAbove)
+                    {
+                        if (columnLeft && columnRight)
+                        {
+                            greenCellsCount += this.allNumbers[column - 1, row - 1];
+                            greenCellsCount += this.allNumbers[column - 1, row];
+                            greenCellsCount += this.allNumbers[column - 1, row + 1];
+                            greenCellsCount += this.allNumbers[column, row - 1];
+                            greenCellsCount += this.allNumbers[column, row + 1];
+                        }
+                        else if (columnLeft)
+                        {
+                            greenCellsCount += this.allNumbers[column - 1, row - 1];
+                            greenCellsCount += this.allNumbers[column - 1, row];
+                            greenCellsCount += this.allNumbers[column, row - 1];
+                        }
+                        else if (columnRight)
+                        {
+                            greenCellsCount += this.allNumbers[column - 1, row];
+                            greenCellsCount += this.allNumbers[column - 1, row + 1];
+                            greenCellsCount += this.allNumbers[column, row + 1];
+                        }
+                    }
+                    else if (rowBelow)
+                    {
+                        if (columnLeft && columnRight)
+                        {
+                            greenCellsCount += this.allNumbers[column, row - 1];
+                            greenCellsCount += this.allNumbers[column, row + 1];
+                            greenCellsCount += this.allNumbers[column + 1, row - 1];
+                            greenCellsCount += this.allNumbers[column + 1, row];
+                            greenCellsCount += this.allNumbers[column + 1, row + 1];
+                        }
+                        else if (columnLeft)
+                        {
+                            greenCellsCount += this.allNumbers[column, row - 1];
+                            greenCellsCount += this.allNumbers[column + 1, row - 1];
+                            greenCellsCount += this.allNumbers[column + 1, row];
+                        }
+                        else if (columnRight)
+                        {
+                            greenCellsCount += this.allNumbers[column, row + 1];
+                            greenCellsCount += this.allNumbers[column + 1, row];
+                            greenCellsCount += this.allNumbers[column + 1, row + 1];
+                        }
+                    }
+
+                    //If current cell is red
+                    if (this.allNumbers[column, row] == 0)
+                    {
+                        if (greenCellsCount == 3 || greenCellsCount == 6)
+                        {
+                            forNextGeneration[column, row] = 1;
+                        }
+                        else
+                        {
+                            forNextGeneration[column, row] = 0;
+                        }
+                    }
+                    //If current cell is green
+                    else
+                    {
+                        if (greenCellsCount != 2 && greenCellsCount != 3 && greenCellsCount != 6)
+                        {
+                            forNextGeneration[column, row] = 0;
+                        }
+                        else
+                        {
+                            forNextGeneration[column, row] = 1;
+                        }
+                    }
+                }
+            }
+
+            Array.Copy(this.forNextGeneration, this.allNumbers, this.forNextGeneration.Length);
+
+            PrintCurrentGeneration();
+
+            
+
+        }
+
+        public void PrintCurrentGeneration()
+        {
+            for (int i = 0; i < this.xLength; i++)
+            {
+                for (int j = 0; j < this.yLength; j++)
+                {
+                    Console.Write(this.allNumbers[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public int CheckForSearchedPoint(int coodinateX, int coodinateY)
+        {
+            if (this.allNumbers[coodinateX, coodinateY] == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Green vs Red OOP/Green vs Red OOP/Start.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Green_vs_Red_OOP
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            //Ask user to enter numbers for grid size
+            Console.WriteLine("Please write the size of the grid in this format x,y (3,9)");
+            string firstLine = Console.ReadLine();
+            string[] tempArray = firstLine.Split(",");
+
+            int xLength = 0;
+            int yLength = 0;
+
+            try
+            {
+                xLength = Int32.Parse(tempArray[0]);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("The " + tempArray[0] + " is not a number");
+            }
+
+            try
+            {
+                yLength = Int32.Parse(tempArray[1]);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("The " + tempArray[1] + " is not a number");
+            }
+
+            if (xLength > 999 || yLength > 999)
+            {
+                Console.WriteLine("The x and y must be lower than 1000");
+                return;
+            }
+
+            //Build the grid
+            Grid currentGrid = new Grid(xLength, yLength);
+            
+            //Setting values in the grid array
+            currentGrid.SetTheGridGenerationZero();
+
+            //Entering coordinates for searched point and number of turns
+            Console.WriteLine("Please write the coordinate of cell to be checked how many times will be green and number of turns x,y,turns (1,2,10)");
+
+            int coordinateX = 0;
+            int coordinateY = 0;
+            int turnsMax = 0;
+
+            string coordinatesLine = Console.ReadLine();
+            tempArray = coordinatesLine.Split(",");
+
+            try
+            {
+                coordinateX = Int32.Parse(tempArray[0]);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("The " + tempArray[0] + " is not a number");
+            }
+
+            try
+            {
+                coordinateY = Int32.Parse(tempArray[1]);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("The " + tempArray[1] + " is not a number");
+            }
+
+            try
+            {
+                turnsMax = Int32.Parse(tempArray[2]);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("The " + tempArray[2] + " is not a number");
+            }
+
+            int turns = 0;
+            int timesSearchedPointIsOne = 0;
+
+            //Making generations and checks for searched point value
+            while (turns < turnsMax)
+            {
+                currentGrid.CalculateNextGeneration();
+                currentGrid.PrintCurrentGeneration();
+                timesSearchedPointIsOne += currentGrid.CheckForSearchedPoint(coordinateX, coordinateY);
+                turns++;
+            }
+
+            Console.WriteLine("The cell " + coordinateX + "," + coordinateY + " was green " + timesSearchedPointIsOne + " times.");
+            
+        }
+    }
+}
